9fe0912fa947b058fa381c9dfd1ce9b2010d5fc4
成龙生 <1536463948@qq.com>
2021-11-16 14:26:14 +0800

    order模块搭建并测试成功

--- mall-order/src/main/java/com/cls/mymall/order/controller/OrderController.java
+++ mall-order/src/main/java/com/cls/mymall/order/controller/OrderController.java
@@ -1,19 +1,14 @@
 package com.cls.mymall.order.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cls.mymall.order.entity.OrderEntity;
-import com.cls.mymall.order.service.OrderService;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.R;
+import com.cls.mymall.order.entity.OrderEntity;
+import com.cls.mymall.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
