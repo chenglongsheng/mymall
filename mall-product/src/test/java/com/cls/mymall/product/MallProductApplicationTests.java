ba5ac524580040b7ce2250c7f267eaca7f88b39b
成龙生 <1536463948@qq.com>
2021-11-16 11:20:07 +0800

    product项目搭建并测试

--- mall-product/src/test/java/com/cls/mymall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/cls/mymall/product/MallProductApplicationTests.java
@@ -1,13 +1,25 @@
 package com.cls.mymall.product;
 
+import com.cls.mymall.product.entity.BrandEntity;
+import com.cls.mymall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MallProductApplicationTests {
 
+    @Autowired
+    private BrandService brandService;
+
     @Test
     void contextLoads() {
+
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setName("小米");
+        brandService.save(brandEntity);
+
     }
 
 }
