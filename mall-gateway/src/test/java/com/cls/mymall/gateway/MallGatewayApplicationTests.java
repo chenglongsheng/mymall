abb5b1fdd091158a670fdf885d267e9e8daa9138
成龙生 <1536463948@qq.com>
2021-11-22 10:45:58 +0800

    gateway配置测试

---
+++ mall-gateway/src/test/java/com/cls/mymall/gateway/MallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cls.mymall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
