c0eb8d5c9492c75c99bcd68f6c6323a4877d05c5
成龙生 <1536463948@qq.com>
2022-01-14 15:44:32 +0800

    变动字段名

--- mall-product/src/main/java/com/cls/mymall/product/entity/BrandEntity.java
+++ mall-product/src/main/java/com/cls/mymall/product/entity/BrandEntity.java
@@ -36,7 +36,7 @@ public class BrandEntity implements Serializable {
     /**
      * 介绍
      */
-    private String descript;
+    private String description;
     /**
      * 显示状态[0-不显示；1-显示]
      */
