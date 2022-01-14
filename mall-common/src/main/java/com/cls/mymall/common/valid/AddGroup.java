f8dd70f49174ba3a1838452d473437f7a94e6c92
成龙生 <1536463948@qq.com>
2022-01-14 21:31:17 +0800

    校验分组标识接口

---
+++ mall-common/src/main/java/com/cls/mymall/common/valid/AddGroup.java
@@ -0,0 +1,7 @@
+package com.cls.mymall.common.valid;
+
+/**
+ * 校验分组新增标识接口
+ */
+public interface AddGroup {
+}
---
+++ mall-common/src/main/java/com/cls/mymall/common/valid/UpdateGroup.java
@@ -0,0 +1,7 @@
+package com.cls.mymall.common.valid;
+
+/**
+ * 校验分组修改标识接口
+ */
+public interface UpdateGroup {
+}
