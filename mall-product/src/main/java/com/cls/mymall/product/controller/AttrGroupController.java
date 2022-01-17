f5c4a724d5998c3b6617ebf64365d44a9b3d2152
成龙生 <1536463948@qq.com>
2022-01-17 21:59:55 +0800

    获取分类属性分组

--- mall-product/src/main/java/com/cls/mymall/product/controller/AttrGroupController.java
+++ mall-product/src/main/java/com/cls/mymall/product/controller/AttrGroupController.java
@@ -1,19 +1,14 @@
 package com.cls.mymall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.cls.mymall.product.entity.AttrGroupEntity;
-import com.cls.mymall.product.service.AttrGroupService;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.R;
+import com.cls.mymall.product.entity.AttrGroupEntity;
+import com.cls.mymall.product.service.AttrGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
@@ -32,10 +27,11 @@ public class AttrGroupController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/{catelogId}")
 //    @RequiresPermissions("product:attrgroup:list")
-    public R list(@RequestParam Map<String, Object> params) {
-        PageUtils page = attrGroupService.queryPage(params);
+    public R list(@RequestParam Map<String, Object> params, @PathVariable Long catelogId) {
+//        PageUtils page = attrGroupService.queryPage(params);
+        PageUtils page = attrGroupService.queryPage(params, catelogId);
 
         return R.ok().put("page", page);
     }
--- mall-product/src/main/java/com/cls/mymall/product/service/impl/AttrGroupServiceImpl.java
+++ mall-product/src/main/java/com/cls/mymall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,18 +1,17 @@
 package com.cls.mymall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.Query;
-
 import com.cls.mymall.product.dao.AttrGroupDao;
 import com.cls.mymall.product.entity.AttrGroupEntity;
 import com.cls.mymall.product.service.AttrGroupService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("attrGroupService")
@@ -22,9 +21,30 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<AttrGroupEntity> page = this.page(
                 new Query<AttrGroupEntity>().getPage(params),
-                new QueryWrapper<AttrGroupEntity>()
+                new QueryWrapper<>()
         );
 
+        return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        String key = (String) params.get("key");
+
+        IPage<AttrGroupEntity> page;
+        if (catelogId == 0) {
+            page = this.page(new Query<AttrGroupEntity>().getPage(params), null);
+        } else {
+            page = this.page(new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<AttrGroupEntity>()
+                            .eq("catelog_id", catelogId)
+                            .and(StringUtils.hasLength(key), (i) -> i
+                                    .eq("attr_group_id", key)
+                                    .or()
+                                    .like("attr_group_name", key)
+                                    .or()
+                                    .like("descript", key)));
+        }
         return new PageUtils(page);
     }
 
