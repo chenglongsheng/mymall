675c469ca072948a185bd8d3c73cc2a2ce5f6871
成龙生 <1536463948@qq.com>
2022-01-19 21:38:46 +0800

    添加属性分类路径

--- mall-product/src/main/java/com/cls/mymall/product/entity/AttrGroupEntity.java
+++ mall-product/src/main/java/com/cls/mymall/product/entity/AttrGroupEntity.java
@@ -1,12 +1,11 @@
 package com.cls.mymall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.io.Serializable;
-import java.util.Date;
-
-import lombok.Data;
 
 /**
  * 属性分组
@@ -45,5 +44,10 @@ public class AttrGroupEntity implements Serializable {
      * 所属分类id
      */
     private Long catelogId;
+    /**
+     * 分类路径
+     */
+    @TableField(exist = false)
+    private Long[] catelogPath;
 
 }
