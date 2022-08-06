0a1d27c1d7e6ae05c34526ef6b9b87900831858f
成龙生 <1536463948@qq.com>
2022-08-06 15:09:27 +0800

    品牌模糊查询

--- mall-product/src/main/java/com/cls/mymall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/cls/mymall/product/service/impl/BrandServiceImpl.java
@@ -1,18 +1,17 @@
 package com.cls.mymall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.Query;
-
 import com.cls.mymall.product.dao.BrandDao;
 import com.cls.mymall.product.entity.BrandEntity;
 import com.cls.mymall.product.service.BrandService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Map;
 
 
 @Service("brandService")
@@ -20,9 +19,14 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
+                        .lambda()
+                        .eq(!StringUtils.isEmpty(key), BrandEntity::getBrandId, key)
+                        .or()
+                        .like(!StringUtils.isEmpty(key), BrandEntity::getName, key)
         );
 
         return new PageUtils(page);
