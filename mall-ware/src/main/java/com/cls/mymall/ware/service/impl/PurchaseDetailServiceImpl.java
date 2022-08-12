5ee9eacdb751d280d9240f926e0bb896b3cd1c4c
成龙生 <1536463948@qq.com>
2022-08-12 09:37:58 +0800

    查询采购需求

--- mall-ware/src/main/java/com/cls/mymall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/com/cls/mymall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,18 +1,17 @@
 package com.cls.mymall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.Query;
-
 import com.cls.mymall.ware.dao.PurchaseDetailDao;
 import com.cls.mymall.ware.entity.PurchaseDetailEntity;
 import com.cls.mymall.ware.service.PurchaseDetailService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import java.util.Map;
 
 
 @Service("purchaseDetailService")
@@ -20,10 +19,12 @@ public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, Pu
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<PurchaseDetailEntity> page = this.page(
-                new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
-        );
+        QueryWrapper<PurchaseDetailEntity> wrapper = new QueryWrapper<>();
+        Object key = params.get("key");
+        Object status = params.get("status");
+        Object wareId = params.get("wareId");
+        wrapper.lambda().eq(!ObjectUtils.isEmpty(key), PurchaseDetailEntity::getId, key).eq(!ObjectUtils.isEmpty(status), PurchaseDetailEntity::getStatus, status).eq(!ObjectUtils.isEmpty(wareId), PurchaseDetailEntity::getWareId, wareId);
+        IPage<PurchaseDetailEntity> page = this.page(new Query<PurchaseDetailEntity>().getPage(params), wrapper);
 
         return new PageUtils(page);
     }
