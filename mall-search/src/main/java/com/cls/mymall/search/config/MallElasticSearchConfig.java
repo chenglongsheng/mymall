a3a625c5dd5d69a8a688194b26cdf2cf84e805ee
成龙生 <1536463948@qq.com>
2022-08-15 09:31:28 +0800

    测试elasticsearch的index操作

--- mall-search/src/main/java/com/cls/mymall/search/config/MallElasticSearchConfig.java
+++ mall-search/src/main/java/com/cls/mymall/search/config/MallElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.cls.mymall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MallElasticSearchConfig {
+
+    public final static RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- mall-search/src/test/java/com/cls/mymall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/cls/mymall/search/MallSearchApplicationTests.java
@@ -1,13 +1,49 @@
 package com.cls.mymall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.cls.mymall.search.config.MallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class MallSearchApplicationTests {
 
+    @Autowired
+    private RestHighLevelClient client;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("user");
+        indexRequest.id("1");
+        User user = new User();
+        user.setUserName("张三");
+        user.setAge(18);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        IndexRequest request = indexRequest.source(jsonString, XContentType.JSON);
+        IndexResponse index = client.index(request, MallElasticSearchConfig.COMMON_OPTIONS);
+
+        System.out.println(index);
+    }
+
+    @Data
+    static
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
 }
