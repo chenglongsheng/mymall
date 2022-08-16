95f49e3a134e2b6ff0e442927f774c2851ff4da6
成龙生 <1536463948@qq.com>
2022-08-16 10:07:25 +0800

    查询商品基本属性

--- mall-product/src/main/java/com/cls/mymall/product/service/impl/ProductAttrValueServiceImpl.java
+++ mall-product/src/main/java/com/cls/mymall/product/service/impl/ProductAttrValueServiceImpl.java
@@ -1,18 +1,17 @@
 package com.cls.mymall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import java.util.Map;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cls.mymall.common.utils.PageUtils;
 import com.cls.mymall.common.utils.Query;
-
 import com.cls.mymall.product.dao.ProductAttrValueDao;
 import com.cls.mymall.product.entity.ProductAttrValueEntity;
 import com.cls.mymall.product.service.ProductAttrValueService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Service("productAttrValueService")
@@ -26,6 +25,11 @@ public class ProductAttrValueServiceImpl extends ServiceImpl<ProductAttrValueDao
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<ProductAttrValueEntity> baseAttrListForSpu(Long spuId) {
+        return super.list(new QueryWrapper<ProductAttrValueEntity>().eq("spu_id", spuId));
     }
 
 }
