f4858a625422913d19c5b9aa41a3ff5ae8f64420
成龙生 <1536463948@qq.com>
2022-08-28 09:59:53 +0800

    测试redisson

--- mall-product/src/test/java/com/cls/mymall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/cls/mymall/product/MallProductApplicationTests.java
@@ -3,6 +3,7 @@ package com.cls.mymall.product;
 import com.cls.mymall.product.entity.BrandEntity;
 import com.cls.mymall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +12,14 @@ class MallProductApplicationTests {
 
     @Autowired
     private BrandService brandService;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Test
+    void testRedisson() {
+        System.out.println(redissonClient);
+    }
 
     @Test
     void contextLoads() {
