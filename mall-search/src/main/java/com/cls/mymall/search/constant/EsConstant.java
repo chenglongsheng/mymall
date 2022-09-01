6cb286da22a8737d628d55e30f2889713f57d51a
成龙生 <1536463948@qq.com>
2022-09-01 16:43:12 +0800

    常量页面数量

--- mall-search/src/main/java/com/cls/mymall/search/constant/EsConstant.java
+++ mall-search/src/main/java/com/cls/mymall/search/constant/EsConstant.java
@@ -3,5 +3,6 @@ package com.cls.mymall.search.constant;
 public class EsConstant {
 
     public static final String PRODUCT_INDEX = "product";
+    public static final Integer PRODUCT_PAGE_SIZE = 2;
 
 }
