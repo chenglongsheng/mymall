d81107f55a4f3b9f27233c8fdaaaea18b8d15b4a
成龙生 <1536463948@qq.com>
2022-09-11 15:19:40 +0800

    删除key

--- mall-third-party/src/test/java/com/cls/mymall/thirdparty/MallThirdPartyApplicationTests.java
+++ mall-third-party/src/test/java/com/cls/mymall/thirdparty/MallThirdPartyApplicationTests.java
@@ -20,8 +20,6 @@ class MallThirdPartyApplicationTests {
 //        // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 //        String endpoint = "https://oss-cn-shenzhen.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5tRUDiyFRUXY7DAsEMDH";
-//        String accessKeySecret = "ehrYuAYOoME6kwdIx46hOeZPoRXknH";
 //
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
